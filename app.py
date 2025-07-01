070d33eee22e1d30d6c79eb1cdef204ad22e66df
Karim Tawfik <127509717+karimtawfikk@users.noreply.github.com>
2025-07-01 23:13:00 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader,  DirectoryLoader
+from langchain.chains.summarize import load_summarize_chain
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
+import torch
+import base64
+
+#Model & Tokenizer
+checkpoint = "MBZUAI/LaMini-Flan-T5-248M"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+base_model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint).to(device)
+
+
+#Summarization Pipeline
+def llm_pipeline(filepath):
+    pipe_sum = pipeline(
+        "summarization",
+        model=base_model,
+        tokenizer=tokenizer,
+        max_length=40, #set to small since summarizing chuncks not the entire text
+        min_length=15
+    )
+
+    loader = PyPDFLoader(filepath)
+    pages = loader.load_and_split()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=50) #each chunk 400 chars, each chunk will overlap the previous one by 50 characters
+    chunks = text_splitter.split_documents(pages) #splits into small chuncks in each page 
+
+    all_summaries = []
+    for chunk in chunks:
+        input_text = chunk.page_content
+        result = pipe_sum(input_text)[0]['summary_text']
+        all_summaries.append(result)
+
+    final_summary = " ".join(all_summaries)
+    return final_summary
+
+#Display PDF Function
+@st.cache_data
+def displayPDF(file):
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
+#Streamlit code
+st.set_page_config(layout="wide", page_title="Summarization App")
+
+def main():
+    st.title("Document Summarization App using Language Model")
+
+    uploaded_file = st.file_uploader("Upload your PDF File", type=["pdf"])
+
+    if uploaded_file is not None:
+        if st.button("Summarize"):
+            col1, col2 = st.columns(2)
+            filepath = "data/" + uploaded_file.name
+
+            with open(filepath, "wb") as temp_file:
+                temp_file.write(uploaded_file.read())
+
+            with col1:
+                st.info("Uploaded PDF File")
+                displayPDF(filepath)
+
+            with col2:
+                st.info("Summarization is below")
+                summary = llm_pipeline(filepath)
+                st.success(summary)
+
+if __name__ == "__main__":
+    main()
